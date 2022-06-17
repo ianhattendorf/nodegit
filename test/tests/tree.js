a7ec5fa0d3c04e819549ce79b6871a7acfea6efd
Ian Hattendorf <ianh@axosoft.com>
2022-06-17 10:12:36 -0700

    Update for knime_workflows

--- test/tests/tree.js
+++ test/tests/tree.js
@@ -8,9 +8,10 @@ describe("Tree", function() {
   var RepoUtils = require("../utils/repository_setup");
 
   var repoPath = local("../repos/tree");
-  var existingPath = local("../../../linux");
+  var existingPath = local("../../../../test/knime_workflows");
   // var oid = "5716e9757886eaf38d51c86b192258c960d9cfea";
-  var oid = "37862e8360dfef539d054c21f3b3cdd3f6728dbd";
+  // var oid = "37862e8360dfef539d054c21f3b3cdd3f6728dbd";
+  var oid = "542ccd930b79b372efdbaee58bbec21bdb829385";
 
   beforeEach(function() {
     var test = this;
@@ -119,13 +120,21 @@ describe("Tree", function() {
     console.time("getAllFilepaths");
     const paths = await tree.getAllFilepaths();
     console.timeEnd("getAllFilepaths");
-    assert.equal(paths.length, 72178);
-    assert.equal(paths[0], ".clang-format");
-    assert.equal(paths[paths.length - 1], "virt/lib/irqbypass.c");
+    console.log({ pl: paths.length, p0: paths[0], pe: paths[paths.length - 1] });
+    assert.equal(paths.length, 73198);
+    assert.equal(paths[0], ".gitignore");
+    assert.equal(paths[paths.length - 1], "team_folder/workflow_parts/sizable table querier/workflow.knime");
   });
 
 });
+// linux
 // std::string 121 + 133 + 122 + 123 + 116 = 615
 // custom buffer 150 + 121 + 114 + 215 + 144 = 744
 // custom buffer 159 + 115 + 136 + 120 + 155 = 685
 // std::string 115 + 191 + 124 + 121 + 113 = 664
+
+// knime_workflows
+// std::string 218 + 226 + 228 + 208 + 310 = 1190
+// std::string 269 + 219 + 217 + 194 + 198 = 1097
+// custom buffer 259 + 213 + 226 + 290 + 198 = 1186
+// custom buffer 266 + 208 + 279 + 213 + 199 = 1165
